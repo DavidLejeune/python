95ea4c8f22dacdb4f77e1134abab3faaefcd9133
Visrut Sudhakar <visrut@suvithra.com>
2015-04-11 15:06:17 -0400

    Donkey kong support as a rectangle

--- boss.py
+++ boss.py
@@ -3,4 +3,9 @@ import pygame
 
 class DK:
     def __init__(self):
-        self.obj = pygame.Rect((200, 300))
+        self.obj = pygame.Rect((0, 0), (50, 70))
+        self.speed = 1
+    def move_right(self):
+        self.obj = self.obj.move((self.speed, 0))
+    def move_left(self):
+        self.obj = self.obj.move((-self.speed, 0))
--- donkeykong.py
+++ donkeykong.py
@@ -5,6 +5,7 @@
 import pygame, sys
 import platform
 import spritesheet
+import boss
 
 ## Colors ##
 BLACK = (0, 0, 0)
@@ -13,21 +14,35 @@ RED = (255, 0, 0)
 
 pygame.init()
 
+FPS = 30
+clock = pygame.time.Clock()
+
 size = (600, 800)
 
 screen = pygame.display.set_mode(size)  # Main surface
 
 background = pygame.Surface(size) # BG Surface
 background = background.convert()
+
 foreground = pygame.Surface(size)
+
 background.fill(BLACK)
 
 # Generate and draw platforms
 levels = platform.Platforms(size, 3)
 foreground = levels.render(foreground)
 
+# Create and add characters
+dk = boss.DK()
+
+# Move controllers
+dk_right = False
+dk_left = False
+
 # Game Loop
 while True:
+    pygame.draw.rect(foreground, (255, 0, 0), dk.obj)
+
     ev = pygame.event.poll()
     if ev.type == pygame.QUIT:
         print "Exit"
@@ -38,8 +53,28 @@ while True:
         print ev.key
         if ev.key == 32:
             levels.iSmash()
-            foreground.fill(BLACK)
+            foreground.fill((0,0,0,0))
             foreground = levels.render(foreground)
+        if ev.key == 275:
+            dk_right = True
+        if ev.key == 276:
+            dk_left = True
+    if ev.type == pygame.KEYUP:
+        print ev.key
+        if ev.key == 275:
+            dk_right = False
+        if ev.key == 276:
+            dk_left = False
+
+    if dk_right:
+        print 'bananas'
+        foreground.fill((0,0,0,0))
+        foreground = levels.render(foreground)
+        dk.move_right()
+    if dk_left:
+        foreground.fill((0,0,0,0))
+        foreground = levels.render(foreground)
+        dk.move_left()
 
     screen.blit(background, (0, 0))
     screen.blit(foreground, (0, 0))
