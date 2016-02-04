8a34f2e3e14ab8a2e2193a4978027812479abbf9
DavidLejeune <Lejeune.David@outlook.com>
2016-02-04 20:46:44 +0100

    Working frame with event logging

--- pygame1.py
+++ pygame1.py
@@ -1,15 +1,27 @@
 import pygame
 
+gameTitle = 'Snake Revamp'
+gameExit = False
+
+
 
 pygame.init()
 x = pygame.init()
 print(x)
 
 gameDisplay = pygame.display.set_mode((800,600))
-
+pygame.display.set_caption('Snake Revamp')
 
 pygame.display.update()
-#{pygame.display.flip()
+#pygame.display.flip()
+
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+
+
+
 
 
 
