ddd2af63b0a2ff35ed6e44a1adbf3b52e61035c2
DavidLejeune <Lejeune.David@outlook.com>
2016-02-04 20:18:59 +0100

    basic framework

---
+++ pygame1.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+pygame.init()
+x = pygame.init()
+print(x)
+
+gameDisplay = pygame.display.set_mode((800,600))
+
+
+pygame.display.update()
+#{pygame.display.flip()
+
+
+
+
+pygame.quit()
+quit()
